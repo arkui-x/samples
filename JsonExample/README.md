3116667b7bca069929eddcc42bcae3ec40a50e62
yuxuewen1 <yuxuewen1@h-partners.com>
2023-10-28 12:51:11 +0800

    add使用不同的库反序列化一组json字符串sample

---
+++ JsonExample/README.md
@@ -0,0 +1,34 @@
+# 反序列化JSON应用示例
+## 简介
+本示例通过[ArkUI-X] Entry Ability模版创建的一个工程,可通过点击Button按钮反序列化JSON字符串展示内容。效果图如下：
+
+* Android平台展示效果
+  ![](./screenshots/devices/android_main.png) ![](./screenshots/devices/android_deserialize.png)
+
+* iOS平台展示效果
+  ![](./screenshots/devices/ios_main.png) ![](./screenshots/devices/ios_deserialize.png)
+
+* OpenHarmony平台展示效果
+  ![](./screenshots/devices/oh_main.png) ![](./screenshots/devices/oh_deserialize.png)
+
+## 相关概念
+
+不涉及
+
+## 相关权限
+
+不涉及。
+
+## 使用说明
+
+1.打开应用，首页面显示一组单选按钮和一组按钮。
+
+2.选择反序列化工具后，点击示例数据或自定义数据
+
+3.点击反序列化按钮，反序列化JSON字符串。
+
+## 约束与限制
+
+1.本示例支持在Android\iOS\OpenHarmony平台上运行。
+
+2.本示例需要使用DevEco Studio 4.0 Beta2及以上版本才可编译运行。
---
+++ JsonExample/entry/src/main/ets/utils/Formatter.ts
@@ -0,0 +1,127 @@
+import { isBasicType } from './Index'
+
+/**
+ * 返回指定个数的制表符字符串
+ * @param count
+ * @returns
+ */
+function setTabs(count: number): string {
+  let str = ''
+  for (let i = 0; i < count; i++) {
+    str += `\t`
+  }
+  return str
+}
+
+/**
+ * 构造数组类型的文本内容
+ * @param arr 被格式化的对象数组
+ * @param tabCount 制表符个数
+ * @returns
+ */
+function convertArray(arr: Array<object>, tabCount: number = 0): string {
+  let formatterJSONArr = ''
+  const arrLen = arr.length
+  if (arrLen === 0) return formatterJSONArr += '[]'
+  const someSimple = arr.some(item => isBasicType(item)) // 判断数组内数据是否存在基础数据类型
+  if (someSimple) {
+    // 如果数组长度大于5，则使用,\n分割，否则只使用,分割
+    if (arrLen >= 5) {
+      formatterJSONArr += `[\n`
+      tabCount++
+      formatterJSONArr += `${setTabs(tabCount)}`
+      arr.forEach((item, index) => {
+        const lastIndex = index === arrLen - 1
+        if (null === item || isBasicType(item)) { // 判断是否为基础数据类型
+          if (typeof item === 'string') {
+            formatterJSONArr += `"${item}"${lastIndex ? '\n' : ',\n'}`
+          } else {
+            formatterJSONArr += `${item}${lastIndex ? '\n' : ',\n'}`
+          }
+        } else {
+          if (item.constructor === Array) { // 判断是否为数组类型
+            formatterJSONArr += convertArray(item, tabCount + 1) // 调用构造数组类型文本内容的方法
+          } else {
+            formatterJSONArr += convertObject(item, tabCount + 1,!lastIndex) // 调用构造对象类型文本内容的方法
+          }
+          formatterJSONArr += `\n`
+        }
+        // 不是最后一个元素则在前面加上制表符的空格
+        if (!lastIndex) {
+          formatterJSONArr += `${setTabs(tabCount)}`
+        }
+      })
+      tabCount--
+      formatterJSONArr += `${setTabs(tabCount)}`
+      formatterJSONArr += `]`
+    } else {
+      formatterJSONArr += `[`
+      arr.forEach((item, index) => {
+        const lastIndex = index === arrLen - 1
+        if (null === item || isBasicType(item)) {
+          if (typeof item === 'string') {
+            formatterJSONArr += `"${item}"${lastIndex ? '' : ','}`
+          } else {
+            formatterJSONArr += `${item}${lastIndex ? '' : ','}`
+          }
+        } else {
+          if (item.constructor === Array) {
+            formatterJSONArr += convertArray(item, tabCount + 1) // 调用构造数组类型文本内容的方法
+          } else {
+            formatterJSONArr += convertObject(item, tabCount + 1,!lastIndex) // 调用构造对象类型文本内容的方法
+          }
+          formatterJSONArr += `${setTabs(tabCount)}`
+        }
+      })
+      formatterJSONArr += `]`
+    }
+  } else {
+    formatterJSONArr = '[\n'
+    tabCount++;
+    arr.forEach((item, index) => {
+      const lastIndex = index === arrLen - 1
+      formatterJSONArr += `${setTabs(tabCount)}`
+      formatterJSONArr += convertObject(item, tabCount,!lastIndex)
+    })
+    tabCount--
+    formatterJSONArr += `${setTabs(tabCount)}]`
+  }
+  return formatterJSONArr
+}
+
+/**
+ * 构造对象类型的文本内容
+ * @param obj 被格式化的对象
+ * @param tabCount 制表符个数
+ * @param addComma 末尾是否需要增加逗号
+ * @returns
+ */
+function convertObject(obj: object, tabCount: number = 0, addComma: boolean = false): string {
+  let formatterJSON: string = '{\n'
+  tabCount++;
+  const keys: Array<string> = Object.keys(obj) // 获取object的key
+  const keysLen: number = keys.length
+  keys.forEach((key: string, index) => {
+    const type = isBasicType(obj[key]) // 根据key获取value，判断value的类型
+    const isStr = typeof obj[key] === 'string'
+    const lastKey = index === keysLen - 1 // 是否为最后一个key
+    if (null === obj[key] || type) { // 如果是简单类型的数据，直接拼接
+      formatterJSON += `${setTabs(tabCount)}"${key}": ${isStr ? '"' + obj[key] + '"' : obj[key]}${lastKey ? '' : ','}\n`
+    } else {
+      // 判断非简单类型的数据是object还是array
+      if (obj[key].constructor === Array) { // 如果是array，调用转化array的方法
+        formatterJSON += `${setTabs(tabCount)}"${key}": ` + convertArray(obj[key], tabCount) + `${lastKey ? '' : ','}\n`
+      } else {
+        formatterJSON += `${setTabs(tabCount)}"${key}":` // 否则调用转化object的方法
+        formatterJSON += convertObject(obj[key], tabCount,!lastKey)
+      }
+    }
+  })
+  tabCount--
+  formatterJSON += `${setTabs(tabCount)}}${addComma ? ',' : ''}\n`
+  return formatterJSON
+}
+
+export {
+  convertArray, convertObject, setTabs
+}
