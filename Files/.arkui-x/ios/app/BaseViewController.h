94605fb36942c1987e0a4bd4ad32b7eac5a0de08
燕军亮 <yanjunliang@h-partners.com>
2024-03-14 07:56:45 +0000

    update Files/.arkui-x/ios/app/BaseViewController.h.
    Signed-off-by: 燕军亮 <yanjunliang@h-partners.com>

--- Files/.arkui-x/ios/app/BaseViewController.h
+++ Files/.arkui-x/ios/app/BaseViewController.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FatherViewController : UIViewController
+@interface BaseViewController : UIViewController
 
 @property (nonatomic, copy) NSString *instanceName;
 
