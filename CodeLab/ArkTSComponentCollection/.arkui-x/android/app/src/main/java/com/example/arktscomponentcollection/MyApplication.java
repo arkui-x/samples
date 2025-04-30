65da051148ec1d3a5a70b716bcace88ef30ce2b5
quguiren <quguiren1@huawei-partners.com>
2025-04-30 13:50:49 +0800

    ArkTSComponentCollection
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

---
+++ CodeLab/ArkTSComponentCollection/.arkui-x/android/app/src/main/java/com/example/arktscomponentcollection/MyApplication.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.arktscomponentcollection;
+
+import ohos.stage.ability.adapter.StageApplication;
+
+/**
+ * Example ace application class, which will load ArkUI-X application instance.
+ * StageApplication is provided by ArkUI-X
+ *
+ * @since 2025-01-07
+ */
+public class MyApplication extends StageApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
